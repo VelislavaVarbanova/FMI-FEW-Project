6940ba4a2e70c38eee17b1104ac958c46c84042f
VelislavaVarbanova <velislava.varbanova@gmail.com>
2024-02-12 21:38:41 +0200

    simple add friends function and displaying the friends list

--- Home/Friends-list/script.js
+++ Home/Friends-list/script.js
@@ -3,31 +3,19 @@ import { get } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database
 import { set } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 
-// const currentUserId = auth.currentUser;
-// console.log(currentUserId);
-// console.log(auth);
-
 function addFriends(user) {
-    const friendFullName = document.querySelector('.search-input').value.trim();
-    console.log(friendFullName);
+    const friendFullName = document.getElementById('add-friends').value.trim();
     const currentUserId = user.uid;
-    console.log("Current User ID:", currentUserId);
 
     get(refDatabase('users'))
     .then(usersSnapshot => {
         let friendId;
         usersSnapshot.forEach(function(userSnapshot) {
         const userData = userSnapshot.val();
-        console.log("here in add func!");
         if (userData && userData["full_name"] === friendFullName) {
             friendId = userSnapshot.key;
-            console.log("Current User Friends ID:", friendId);
-            console.log(userData);
-            console.log(userData["full name"]);
-            console.log(friendFullName);
         }
-      });
-    
+        });
         if (friendId) {
           // Friend found, add to friends list
           const friendsToAdd = {};
@@ -50,20 +38,18 @@ function addFriends(user) {
 
 function displayFriends(user) {
     const currentUserId = user.uid;
-    console.log("Current User ID:", currentUserId);
     if (currentUserId) {
     
       // Get reference to user's friends node in the database
-      const friendsRef = refDatabase('/friends/' + currentUserId);
-      console.log("Current User Friends ID:", friendsRef.uid);
+      const friendsRef = refDatabase('friends/' + currentUserId);
       // Fetch user's friends from the database
       get(friendsRef)
       .then(snapshot => {
         const friends = snapshot.val();
         if (friends) {
-          // Assuming you have a DOM element with id 'friends-list' to display the list of friends
-          const friendsListElement = document.getElementById('friends-list');
-        //   friendsListElement.innerHTML = ''; // Clear existing content
+            // Assuming you have a DOM element with id 'friends-list' to display the list of friends
+            const friendsListElement = document.getElementById('friends-list');
+            friendsListElement.innerHTML = ''; // Clear existing content
           
           for (const friendId in friends) {
             // Fetch friend's information from '/users' node in the database
@@ -73,10 +59,13 @@ function displayFriends(user) {
                 const friendData = friendSnapshot.val();
                 if (friendData) {
                   // Display friend's full name
-                  const fullName = friendData["full name"];
-                  const listItem = document.createElement('li');
-                  listItem.textContent = fullName;
-                  friendsListElement.appendChild(listItem);
+                    const fullName = friendData["full_name"];
+                    const listItem = document.createElement('li');
+                    // const iconElement = document.createElement('i');
+                    // iconElement.classList.add('ri-user-3-line');
+                    // listItem.appendChild(iconElement);
+                    listItem.textContent = fullName;
+                    friendsListElement.appendChild(listItem);
                 }
               })
               .catch(error => {
