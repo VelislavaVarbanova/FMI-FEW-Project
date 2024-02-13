fd8f6ab96150afad89fe638dbfe1f695c893cb02
VelislavaVarbanova <velislava.varbanova@gmail.com>
2024-02-13 08:11:14 +0200

    accepting friend request fixed bug

--- Home/Friends-list/script.js
+++ Home/Friends-list/script.js
@@ -1,5 +1,5 @@
-import { auth, onAuthChanged, database, refDatabase } from "../../firebase.js";
-import { get, push, update } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { auth, onAuthChanged, refDatabase } from "../../firebase.js";
+import { get, push, set } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 
 
@@ -46,7 +46,6 @@ function displayFriends(user) {
     const currentUserId = user.uid;
     if (currentUserId) {
     
-      // Get reference to user's friends node in the database
       const friendsRef = refDatabase('friends/' + currentUserId);
       // Fetch user's friends from the database
       get(friendsRef)
@@ -93,62 +92,60 @@ function displayFriends(user) {
 function displayFriendRequests(user) {
   const currentUserId = user.uid;
 
-  // Reference to the friend requests node for the current user
   const friendRequestsRef = refDatabase('friendRequests/' + currentUserId);
 
-  // Fetch friend requests from the database
   get(friendRequestsRef).then(friendRequestsSnapshot => {
       const friendRequests = friendRequestsSnapshot.val();
 
-      // Clear existing content in the friend requests section
       const friendRequestsSection = document.querySelector('.friend-requests-section');
       friendRequestsSection.innerHTML = '';
       
       const requestsTitle = document.createElement('h4');
       requestsTitle.textContent = 'Friend Requests: ';
-      requestsTitle.classList.add('friend-requesrs-title');
+      requestsTitle.classList.add('friend-requests-title');
       friendRequestsSection.appendChild(requestsTitle);
 
-      // Check if there are any friend requests
       if (friendRequests) {
-          // Iterate over each friend request
           for (const friendRequestId in friendRequests) {
               const friendRequestData = friendRequests[friendRequestId];
 
-              const senderId = friendRequestData.sender;
-
-              // Fetch sender's data from the database
-              const senderRef = refDatabase('users/' + senderId);
-              get(senderRef).then(senderSnapshot => {
+              if (friendRequestData.status === 'pending') {
+                const senderId = friendRequestData.sender;
+      
+                // Fetch sender's data from the database
+                const senderRef = refDatabase('users/' + senderId);
+                get(senderRef).then(senderSnapshot => {
                   const senderData = senderSnapshot.val();
                   if (senderData) {
-                      // Create a list item to display the friend request
-                      const listItem = document.createElement('li');
-                      listItem.textContent = `Sender: ${senderData.full_name}, Status: ${friendRequestData.status}`;
-                        
-                      // Create buttons for accepting and rejecting the friend request
-                      const acceptButton = document.createElement('button');
-                      acceptButton.textContent = 'Accept';
-                      const rejectButton = document.createElement('button');
-                      rejectButton.textContent = 'Reject';
-
-                        // Add event listeners to the accept and reject buttons
-                      acceptButton.addEventListener('click', () => {
-                          updateFriendRequestStatus(friendRequestId, 'accepted');
-                      });
-                      rejectButton.addEventListener('click', () => {
-                          updateFriendRequestStatus(friendRequestId, 'rejected');
-                      });
-
-                        // Append buttons to the list item
-                      listItem.appendChild(acceptButton);
-                      listItem.appendChild(rejectButton);
-                      // Append the list item to the friend requests section
-                      friendRequestsSection.appendChild(listItem);
+                    // Create a list item to display the friend request
+                    const listItem = document.createElement('li');
+                    listItem.textContent = `Sender: ${senderData.full_name}, Status: ${friendRequestData.status}`;
+                      
+                    // Create buttons for accepting and rejecting the friend request
+                    const acceptButton = document.createElement('button');
+                    acceptButton.textContent = 'Accept';
+                    const rejectButton = document.createElement('button');
+                    rejectButton.textContent = 'Reject';
+      
+                    // Add event listeners to the accept and reject buttons
+                    acceptButton.addEventListener('click', () => {
+                      updateFriendRequestStatus(friendRequestId, 'accepted', currentUserId);
+                    });
+                    rejectButton.addEventListener('click', () => {
+                      updateFriendRequestStatus(friendRequestId, 'rejected', currentUserId);
+                    });
+      
+                    // Append buttons to the list item
+                    listItem.appendChild(acceptButton);
+                    listItem.appendChild(rejectButton);
+      
+                    // Append the list item to the friend requests section
+                    friendRequestsSection.appendChild(listItem);
                   }
               }).catch(error => {
                   console.error("Error fetching sender data: ", error);
               });
+            }
           }
       } else {
           // If there are no friend requests, display a message
@@ -162,68 +159,74 @@ function displayFriendRequests(user) {
   });
 }
 
-function updateFriendRequestStatus(friendRequestId, status) {
-  console.log("Friend Request ID:", friendRequestId);
+function updateFriendRequestStatus(friendRequestId, status, chatRequestId) {
+    console.log("Friend Request ID:", friendRequestId);
 
-  // Reference to the friend request node in the database
-  const friendRequestRef = refDatabase('friendRequests/' + friendRequestId);
-  console.log("Friend Request Reference:", friendRequestRef.toString());
+    // Reference to the friend request node in the database
+    const friendRequestRef = refDatabase('friendRequests/' + chatRequestId + '/' + friendRequestId);
+    console.log("Friend Request Reference:", friendRequestRef.toString());
 
-  // Fetch the friend request data
-  get(friendRequestRef)
-      .then(friendRequestSnapshot => {
-          const friendRequestData = friendRequestSnapshot.val();
-          console.log("Friend Request Snapshot:", friendRequestSnapshot.val());
-          if (friendRequestData) {
-              const receiverId = friendRequestData.receiver;
-              const senderId = friendRequestData.sender;
+    // Fetch the friend request data
+    get(friendRequestRef)
+        .then(friendRequestSnapshot => {
+            const friendRequestData = friendRequestSnapshot.val();
+            console.log("Friend Request Snapshot:", friendRequestSnapshot.val());
+            if (friendRequestData) {
+                const receiverId = friendRequestData.receiver;
+                const senderId = friendRequestData.sender;
 
-              console.log("Receiver ID:", receiverId);
-              console.log("Sender ID:", senderId);
+                // Construct the new friend request data
+                const newFriendRequestData = {
+                    receiver: receiverId,
+                    sender: senderId,
+                    status: status
+                };
 
-              // Update the status of the friend request
-              update(friendRequestRef, { status: status })
-                  .then(() => {
-                      console.log("Friend request status updated successfully!");
+                // Update the friend request node with the new data
+                set(friendRequestRef, newFriendRequestData)
+                    .then(() => {
+                        console.log("Friend request status updated successfully!");
 
-                      // If the friend request is accepted, add the users as friends
-                      if (status === 'accepted') {
-                          // Add the friend to the receiver's friends list
-                          update(refDatabase('friends/' + receiverId + '/' + senderId), true)
-                              .then(() => {
-                                  console.log("Friend added to receiver's friends list successfully!");
-                                  // Refresh friend requests after updating status
-                                  displayFriendRequests(auth.currentUser);
-                              })
-                              .catch(error => {
-                                  console.error("Error adding friend to receiver's friends list: ", error);
-                              });
+                        // If the friend request is accepted, add the users as friends
+                        if (status === 'accepted') {
+                            // Add the friend to the receiver's friends list
+                            const receiverFriendRef = refDatabase('friends/' + receiverId + '/' + senderId);
+                            set(receiverFriendRef, true)
+                                .then(() => {
+                                    console.log("Friend added to receiver's friends list successfully!");
+                                    // Refresh friend requests after updating status
+                                    // displayFriendRequests(auth.currentUser);
+                                })
+                                .catch(error => {
+                                    console.error("Error adding friend to receiver's friends list: ", error);
+                                });
 
-                          // Add the receiver to the sender's friends list
-                          update(refDatabase('friends/' + senderId + '/' + receiverId), true)
-                              .then(() => {
-                                  console.log("Receiver added to sender's friends list successfully!");
-                                  // Refresh friend requests after updating status
-                                  displayFriendRequests(auth.currentUser);
-                              })
-                              .catch(error => {
-                                  console.error("Error adding receiver to sender's friends list: ", error);
-                              });
-                      } else {
-                          // Refresh friend requests after updating status
-                          displayFriendRequests(auth.currentUser);
-                      }
-                  })
-                  .catch(error => {
-                      console.error("Error updating friend request status: ", error);
-                  });
-          } else {
-              console.error("Friend request data not found.");
-          }
-      })
-      .catch(error => {
-          console.error("Error fetching friend request data: ", error);
-      });
+                            // Add the receiver to the sender's friends list
+                            const senderFriendRef = refDatabase('friends/' + senderId + '/' + receiverId);
+                            set(senderFriendRef, true)
+                                .then(() => {
+                                    console.log("Receiver added to sender's friends list successfully!");
+                                    // Refresh friend requests after updating status
+                                    displayFriendRequests(auth.currentUser);
+                                })
+                                .catch(error => {
+                                    console.error("Error adding receiver to sender's friends list: ", error);
+                                });
+                        } else {
+                            // Refresh friend requests after updating status
+                            displayFriendRequests(auth.currentUser);
+                        }
+                    })
+                    .catch(error => {
+                        console.error("Error updating friend request status: ", error);
+                    });
+            } else {
+                console.error("Friend request data not found.");
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching friend request data: ", error);
+        });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
